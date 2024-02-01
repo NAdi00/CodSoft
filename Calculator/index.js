176d6258c33278fc3eb0de3852f73f3adda6aec5
Adi <110300127+Nemukula@users.noreply.github.com>
2024-02-01 11:59:59 +0200

    Update index.js

--- Calculator/index.js
+++ Calculator/index.js
@@ -64,6 +64,107 @@ function divide() {
 
 ///////////////////////ANSWER
 
-function answer() {
-    console.log(Number(current));
+var signing = 1;
+var count  = 0
+function sign() {
+    count += 1;
+    if (count % 2 == 0) {
+        signing = 1;
+    }else {
+        signing = -1;
+    }
 }
+
+var temp = "43746*87437-344378+43743-72476";
+var my_array = [];
+
+var check = false;
+function checking(object) {
+    for (const i in object) {
+        if (object[i] == "*" || object[i] == "/" || object[i] == "+" || object[i] == "-") {
+            check = true;
+            return;
+        }
+    }
+    check = false;
+    return
+}
+
+function cutting(object) {
+    let object1 = "";
+    for (const i in object) {
+        if (object[i] == "*" || object[i] == "/" || object[i] == "+" || object[i] == "-") {
+            if (signing == -1) {
+                object1 = Number(object.slice(0, i))*-1;
+                if (object[i] == "-") {
+                    signing = -1;
+                }else {
+                    signing = 1;
+                }
+            }
+            else {
+                object1 = object.slice(0, i);
+                if (object[i] == "-") {
+                    signing = -1;
+                }else {
+                    signing = 1;
+                }
+            }
+            my_array.push(Number(object1));
+            my_array.push(object[i]);
+            let index = Number(i) + 1; 
+            temp = object.slice(index);
+            return
+            
+        }
+    }
+}
+
+for(let i = 0; i < 10; i++) {
+    checking(temp);
+    if (check) {
+        cutting(temp);
+    }
+}
+cutting(temp + "*");
+my_array.pop(my_array[-1]);
+
+var check1 = false;
+
+function checking_M(object) {
+    for (const i in object) {
+        if (object[i] == "*") {
+            check1 = true;
+            return
+        }
+    }
+    check1 = false;
+    return
+}
+
+for (let i = 0; i < 5; i++) {
+    checking_M(my_array)
+    console.log(check1);
+}
+
+
+function multi(my_array) {
+    let index1 = my_array.indexOf("*");
+    let a = index1-1;
+    let b = index1+1;
+    let current1 = my_array[a]*my_array[b];
+    my_array[a] = current1;
+    let c = b+1;
+    my_array = my_array.slice(0, index1) +[" ", " "]+ my_array.slice(c);
+    console.log(my_array);
+    return
+}
+for (let i = 0; i < my_array.length; i++) {
+    checking_M(my_array);
+    if (check1) {
+        multi(my_array);
+    }
+}
+cutting(temp);
+console.log(temp, 'after');
+console.log(my_array);
